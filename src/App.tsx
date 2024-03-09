d11302b797503e8de1175760b2969467da8b9ac2
byeongminn <sekn1997@naver.com>
2024-03-09 16:01:20 +0900

    feat: 라우팅 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { About, Contact, Main, Projects } from 'pages';
+import { Navigate, Route, Routes } from 'react-router-dom';
+
 function App() {
-  return <div>Portfolio</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Main />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/projects" element={<Projects />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="*" element={<Navigate replace to="/" />} />
+    </Routes>
+  );
 }
 
 export default App;
---
+++ src/pages/index.ts
@@ -0,0 +1,4 @@
+export { default as Main } from './Main/Main';
+export { default as About } from './About/About';
+export { default as Projects } from './Projects/Projects';
+export { default as Contact } from './Contact/Contact';
