342f10c9f68e5a9091fc3b1d4d71764cc62068b5
byeongminn <sekn1997@naver.com>
2025-01-10 15:18:04 +0900

    docs: modify introduction.json

--- src/components/Introduction/index.tsx
+++ src/components/Introduction/index.tsx
@@ -3,7 +3,7 @@ import { fetchData } from '@/utils/fetcher';
 import { useQuery } from 'react-query';
 
 export const Introduction = () => {
-  const { data: introduction } = useQuery<IntroductionType>(
+  const { data: introductions } = useQuery<IntroductionType[]>(
     'introduction',
     () => fetchData('introduction'),
     {
@@ -17,14 +17,22 @@ export const Introduction = () => {
         Introduction
       </h2>
       <div className="flex flex-col gap-y-6">
-        <div className="flex flex-col laptop:flex-row gap-y-1 laptop:gap-x-3 laptop:gap-y-0 text-gray-800 dark:text-gray-500 break-keep">
-          <h4 className="flex-1 font-spoqa font-medium text-base laptop:text-[20px] laptop:leading-[30px] tracking-[-0.5px]">
-            {introduction?.title}
-          </h4>
-          <p className="flex-1 grow-3 text-kor-14 laptop:font-spoqa laptop:font-normal laptop:text-[18px] laptop:leading-[30px]">
-            {introduction?.content}
-          </p>
-        </div>
+        {introductions?.map((introduction) => (
+          <div
+            key={introduction.id}
+            className="flex flex-col gap-y-1 laptop:gap-y-3 text-gray-800 dark:text-gray-500 break-keep"
+          >
+            <h4 className="flex-1 font-spoqa font-medium text-base laptop:text-[20px] laptop:leading-[30px] tracking-[-0.5px]">
+              {introduction?.title}
+            </h4>
+            <p
+              className="flex-1 grow-3 text-kor-14 laptop:font-spoqa laptop:font-normal laptop:text-[18px] laptop:leading-[30px]"
+              dangerouslySetInnerHTML={{ __html: introduction.content }}
+            />
+            {/* {introduction?.content}
+            </p> */}
+          </div>
+        ))}
       </div>
     </div>
   );
